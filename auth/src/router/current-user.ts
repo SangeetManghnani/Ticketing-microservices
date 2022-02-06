35585438f10ece2548c48e7f51054d8f15dfc74c
Sangeet Manghnani <smanghna@in.ibm.com>
2022-02-06 23:56:29 +0530

    feat: current user, unauthorized middlewares Signed-off-by: Sangeet Manghnani <smanghna@in.ibm.com>

--- auth/src/router/current-user.ts
+++ auth/src/router/current-user.ts
@@ -1,22 +1,11 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-	//1. check if the req.session.jwt is presemt
-	const userJWT = req.session?.jwt;
-	if(!userJWT){
-		return res.send({ currentUser: null });
-	}
-	//2. check  if jwt is valid
-	try{
-		const payload = jwt.verify(userJWT, process.env.JWT_KEY!);
-		//3. return user creds if it is valid.
-		return res.send({ currentUser: payload });
-	} catch(err) {
-		return res.send({ currentUser: null });
-	}
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+	res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
